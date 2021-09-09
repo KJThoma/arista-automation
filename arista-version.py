006d7539ad5693f8c4deb2ae55293f8adcca75df
Kyle Thoma <kyle.j.thoma@gmail.com>
2021-09-09 17:43:55 -0400

    Adding lines to connect multiple devices

--- arista-version.py
+++ arista-version.py
@@ -1,23 +1,34 @@
 from netmiko import ConnectHandler
 
-# our device
-arista_EOS = {
+# our devices info to connect and session log files to save output
+arista_1 = {
     'device_type': 'arista_eos',
     'ip': 'your_ip',
     'username': 'your_username',
-    'password': 'your_password'
+    'password': 'your_password',
+    'session_log': 'sw1.txt'
 }
 
-# define connection
-net_connect = ConnectHandler(**arista_EOS)
+arista_2 = {
+    'device_type': 'arista_eos',
+    'ip': 'your_ip',
+    'username': 'your_username',
+    'password': 'your_password',
+    'session_log': 'sw2.txt'
+}
 
-# send show version command and nicely print output
-output = net_connect.send_command('show version')
-print()
-print('#' * 70)
-print(output)
-print('#' * 70)
-print()
+# command to run on switches
+command = "show version"
+
+# for loop to connect to devices, run on both switches, and print output
+for device in (arista_1, arista_2):
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_command(command)
+    print()
+    print('#' * 70)
+    print(output)
+    print('#' * 70)
+    print()
 
 # gracefully end connection
 net_connect.disconnect()
