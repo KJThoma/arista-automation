ab3754d30f4e4be3428d3425af74163135958b1f
Kyle Thoma <kyle.j.thoma@gmail.com>
2021-09-17 11:11:39 -0400

    Adding account add script and updating README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # arista-automation
-Some Python scripts using netmiko to work with Arista devices
+Some example Python scripts using netmiko to work with Arista devices. Examples in this repo are of showing devices version, and adding an account to switches config.
+# Sources
+https://github.com/ktbyers/netmiko
+
+https://pynet.twb-tech.com
+
+Python for Network Engineers Udemy course by David Bombal
---
+++ add-oxidized-account.py
@@ -0,0 +1,43 @@
+from netmiko import ConnectHandler
+
+# our devices info to connect and session log files to save output
+arista_1 = {
+    'device_type': 'arista_eos',
+    'ip': 'your_ip',
+    'username': 'your_username',
+    'password': 'your_password',
+    'session_log': 'sw1.txt'
+}
+
+arista_2 = {
+    'device_type': 'arista_eos',
+    'ip': 'your_ip',
+    'username': 'your_username',
+    'password': 'your_password',
+    'session_log': 'sw2.txt'
+}
+
+arista_3 = {
+    'device_type': 'arista_eos',
+    'ip': 'your_ip',
+    'username': 'your_username',
+    'password': 'your_password',
+    'session_log': 'sw3.txt'
+}
+
+# command to run on switches
+commands = ['username oxidized privilege 15 role network-admin secret sha512 <secret>']
+
+# for loop to connect to devices in enabled mode, run on switches, write config, and print output
+for device in (arista_1, arista_2, arista_3):
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_config_set(commands)
+    output += net_connect.save_config()
+    print()
+    print('#' * 70)
+    print(output)
+    print('#' * 70)
+    print()
+
+# gracefully end connection
+net_connect.disconnect()
